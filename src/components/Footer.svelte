2725885238c102f8f5b7d38897dc8be6ee5a281c
applepykim <applepykim@naver.com>
2022-06-23 21:30:54 +0900

    Create project

---
+++ src/components/Footer.svelte
@@ -0,0 +1,27 @@
+<footer>
+    <div>
+        <a href="https://github.com/HeropCode/Svelte-Movie-app">
+        GitHub Repo.
+    </a>
+    </div>
+    <div>
+        <a href="https://competent-cori-258206.netlify.app/">
+        @{new Date().getFullYear()}
+        HEROPY
+    </a>
+    </div>
+</footer>
+
+<style lang="scss">
+    footer {
+      padding: 70px;
+      text-align: center;
+      a {
+        color: $color--white-10;
+        text-decoration: none;
+        &:hover {
+          text-decoration: underline;
+        }
+      }
+    }
+</style>
